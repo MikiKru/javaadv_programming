a8000d67987835be7db182e069eb105fea519f72
MikiKru <michal_kruczkowski@o2.pl>
2020-07-25 13:57:23 +0200

    jadv_programming v9

--- src/main/java/z8/Parcel.java
+++ src/main/java/z8/Parcel.java
@@ -1,68 +1,14 @@
 package z8;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Parcel {
     private int xLength;
     private int yLength;
     private int zLength;
     private float weight;
     private boolean isExpress;
-
-    public Parcel(int xLength, int yLength, int zLength, float weight, boolean isExpress) {
-        this.xLength = xLength;
-        this.yLength = yLength;
-        this.zLength = zLength;
-        this.weight = weight;
-        this.isExpress = isExpress;
-    }
-
-    @Override
-    public String toString() {
-        return "Parcel{" +
-                "xLength=" + xLength +
-                ", yLength=" + yLength +
-                ", zLength=" + zLength +
-                ", weight=" + weight +
-                ", isExpress=" + isExpress +
-                '}';
-    }
-
-    public int getxLength() {
-        return xLength;
-    }
-
-    public void setxLength(int xLength) {
-        this.xLength = xLength;
-    }
-
-    public int getyLength() {
-        return yLength;
-    }
-
-    public void setyLength(int yLength) {
-        this.yLength = yLength;
-    }
-
-    public int getzLength() {
-        return zLength;
-    }
-
-    public void setzLength(int zLength) {
-        this.zLength = zLength;
-    }
-
-    public float getWeight() {
-        return weight;
-    }
-
-    public void setWeight(float weight) {
-        this.weight = weight;
-    }
-
-    public boolean isExpress() {
-        return isExpress;
-    }
-
-    public void setExpress(boolean express) {
-        isExpress = express;
-    }
 }
--- src/main/java/z8/ValidatiorImpl.java
+++ src/main/java/z8/ValidatiorImpl.java
@@ -4,18 +4,15 @@ public class ValidatiorImpl implements Validator {
     @Override
     public boolean validate(Parcel input) {
         boolean flag = false;
-        if((input.getxLength() + input.getyLength() + input.getzLength()) < 300) {
-            if (input.getxLength() > 30 && input.getyLength() > 30 && input.getzLength() > 30) {
+        if((input.getXLength() + input.getYLength() + input.getZLength()) < 300) {
+            if (input.getXLength() > 30 && input.getYLength() > 30 && input.getZLength() > 30) {
                 if (input.getWeight() < 30.0 && !input.isExpress()) {
                     flag = true;
                 } else if (input.getWeight() < 15.0 && input.isExpress()) {
                     flag = true;
-                }
-            }
-        }
+                } } }
         return flag;
     }
-
     public static void main(String[] args) {
         ValidatiorImpl v = new ValidatiorImpl();
         System.out.println(v.validate(new Parcel(50,50,50,10, true)));
