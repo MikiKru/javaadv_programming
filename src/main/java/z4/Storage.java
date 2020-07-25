e8a70090bd9159643a0c3ec94cb58aa243e4f775
MikiKru <michal_kruczkowski@o2.pl>
2020-07-25 11:24:36 +0200

    jadv_programming v4

---
+++ src/main/java/z4/Storage.java
@@ -0,0 +1,34 @@
+package z4;
+
+import java.util.*;
+
+// ALT + Ins -> generate
+public class Storage {
+    private Map<String, List<String>> products;
+    public Storage(Map<String, List<String>> products) {
+        this.products = products;
+    }
+    private void addToStorage(String key, String value){
+//        if(!products.containsKey(key)){
+//            products.put(key, new ArrayList<>(Arrays.asList(value)));
+//        } else {
+//            List<String> currentProducts = products.get(key);       // metoda pobiera do listy wartości pod kluczem key
+//            currentProducts.add(value);
+//            products.put(key, currentProducts);
+//        }
+        products.computeIfAbsent(key, k -> new ArrayList<>()).add(value);
+        System.out.println(products);
+    }
+    private void printValues(String key){
+        products.get(key).forEach(System.out::println);
+    }
+    public static void main(String[] args) {
+        Storage storage = new Storage(new HashMap<>());
+        storage.addToStorage("1","P1");
+        storage.addToStorage("2","P2");
+        storage.addToStorage("1","P3");
+        storage.addToStorage("3","P4");
+        storage.addToStorage("1","P5");
+        storage.printValues("1");
+    }
+}
